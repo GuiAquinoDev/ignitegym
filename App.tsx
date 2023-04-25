f6bd3808ccc47642bed9881a7fab33c2ca9a7845
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-25 09:23:47 -0300

    feat: customizando o default theme e o componente Loading

--- App.tsx
+++ App.tsx
@@ -5,13 +5,15 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { Loading } from "@components/Loading";
+
+import { THEME } from './src/themes'
+import { Loading } from "@components/Loading";  
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={ THEME }>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
