7750f66f8ce251a912145334f5ead66f49a619ce
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-26 10:29:42 -0300

    feat: configurando estilo para caso de glitch em rota

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { Box } from 'native-base';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
-    return (
-        <NavigationContainer>
-            <AuthRoutes/>
-        </NavigationContainer>
-    )
+
+  return (
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer >
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
+  );
 }
