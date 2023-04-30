16a99c1b6bf9a50cab18b5328267cb84fb53d6c9
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-30 16:17:28 -0300

    feat: Finalizando HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,12 @@
-import { HStack, Text, Heading, VStack } from "native-base";
+import { HStack, Text, Heading, VStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader() {
   return (
-    <HStack bg="gray.600" pt={16} pb={5} alignItems="center">
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
         source={{
           uri: "https://github.com/guiaquinodev.png",
@@ -12,15 +15,19 @@ export function HomeHeader() {
         size={16}
         mr={4}
       />
-      <VStack>
+      <VStack flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
         <Heading color="gray.100" fontSize="md">
-          Gui
+          Guilherme
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
