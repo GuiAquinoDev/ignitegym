0a8204af3d51b844be7494a0ae9555a87262a5d9
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-30 16:06:09 -0300

    feat: Adicionando imagem de usuario

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,17 @@
 import { HStack, Text, Heading, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} alignItems="center">
+      <UserPhoto
+        source={{
+          uri: "https://github.com/guiaquinodev.png",
+        }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
