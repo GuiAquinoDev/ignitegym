af677ddfc2d4f88f2bdc8c804c2139a52a1c109a
GuiAquinoDev <guiiaquino92@gmail.com>
2023-04-30 17:07:17 -0300

    feat: Adicionando e estilizando o evento Pressed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,16 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { Center, HStack, Text, VStack } from "native-base";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group name="costas" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   );
 }
