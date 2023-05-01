5232822f5d514856204fb24891e782f9d4353827
GuiAquinoDev <guiiaquino92@gmail.com>
2023-05-01 07:59:52 -0300

    feat: Otimizando a exibição do componente group e estilizando

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,27 +2,32 @@ import { useState } from "react";
 import { Group } from "@components/Group";
 
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, HStack, Text, VStack } from "native-base";
+import { Center, HStack, Text, VStack, FlatList } from "native-base";
 
 export function Home() {
+  const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "tríceps"]);
   const [groupSelected, setGroupSelected] = useState("costa");
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <Group
-          name="costas"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-        <Group
-          name="ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
