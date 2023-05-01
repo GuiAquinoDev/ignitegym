12552f5e6ec9a5780687bf6f3a6fc8e4efafcd9b
GuiAquinoDev <guiiaquino92@gmail.com>
2023-05-01 08:49:21 -0300

    feat: criando o header da lista de exercicios e estilizando

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Group } from "@components/Group";
 
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, HStack, Text, VStack, FlatList } from "native-base";
+import { Center, HStack, Text, VStack, FlatList, Heading } from "native-base";
 
 export function Home() {
   const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "tríceps"]);
@@ -28,6 +28,18 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
