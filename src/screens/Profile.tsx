f86ff9bc4f3b2b83829064dfdfb99749e09ce2fb
GuiAquinoDev <guiiaquino92@gmail.com>
2023-06-01 10:13:27 -0300

    feat: Adicionando inputs no perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base'
+
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
-import { useState } from 'react';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 const PHOTO_SIZE = 33;
 
@@ -30,8 +34,56 @@ export function Profile() {
                             size={33}
                         />
                     }
+
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={2}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nome"
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Email"
+                        isDisabled
+                    />
                 </Center>
+                
             </ScrollView>
+
+            <VStack px={10} mt={12} mb={9}>
+                <Heading color="gray.200" fontSize="md" mb={2}>
+                    Alterar senha
+                </Heading>
+
+                <Input 
+                    bg="gray.600"
+                    placeholder="Senha antiga"
+                    secureTextEntry
+                />
+
+                <Input 
+                    bg="gray.600"
+                    placeholder="Nova senha"
+                    secureTextEntry
+                />
+
+
+                <Input 
+                    bg="gray.600"
+                    placeholder="Confirmar senha"
+                    secureTextEntry
+                />
+
+                <Button
+                    title="Atualizar"
+                    mt={4}
+                />
+            </VStack>
        </VStack>
     )
 }
