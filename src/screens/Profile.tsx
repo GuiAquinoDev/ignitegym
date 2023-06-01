1ff602c3b55b7a879aafa3f8b403b70d10cc1309
GuiAquinoDev <guiiaquino92@gmail.com>
2023-06-01 09:32:19 -0300

    feat: Criando estrutura do componente perfil e add skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,37 @@
-import { Center, Text } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
+import { useState } from 'react';
+
+const PHOTO_SIZE = 33;
+
 
 export function Profile() {
+
+    const [photoIsLoading, setPhotoIsLoading] = useState(true);
+
     return (
-        <Center flex={1}>
-            <Text>Home</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil" />
+
+            <ScrollView >
+                <Center mt={6} px={10} >
+                    {
+                        photoIsLoading ?
+                    
+                        <Skeleton w={PHOTO_SIZE} h={PHOTO_SIZE} rounded="full"
+                            startColor="gray.400"
+                            endColor="gray.300"
+                        />
+                        :
+                        <UserPhoto
+                            source={{ uri: 'https://github.com/guiaquinodev.png' }}
+                            alt='Foto do usuario'
+                            size={33}
+                        />
+                    }
+                </Center>
+            </ScrollView>
+       </VStack>
     )
 }
